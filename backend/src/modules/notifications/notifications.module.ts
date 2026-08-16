86851dd647c3d987a32712ac5212984a7706177f
Andy Alejandro <andy.alejandrovera4290@upse.edu.ec>
2026-08-16 10:09:58 -0500

    fix(typeorm): register IncidentEntity in NotificationsModule to resolve FK metadata
    TypeORM requires all entities involved in relations to be registered in forFeature().
    Notification has @ManyToOne to IncidentEntity, so it must be in the feature list.
    
    Fixes: Entity metadata for Notification#incident was not found

--- backend/src/modules/notifications/notifications.module.ts
+++ backend/src/modules/notifications/notifications.module.ts
@@ -5,9 +5,10 @@ import { NotificationsController } from './notifications.controller';
 import { Notification } from './entities/notification.entity';
 import { IncidentNotificationsListener } from './listeners/incident-notifications.listener';
 import { UsersModule } from '../users/users.module';
+import { IncidentEntity } from '../../entities/incident.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Notification]), UsersModule],
+  imports: [TypeOrmModule.forFeature([Notification, IncidentEntity]), UsersModule],
   controllers: [NotificationsController],
   providers: [NotificationsService, IncidentNotificationsListener],
   exports: [NotificationsService],
