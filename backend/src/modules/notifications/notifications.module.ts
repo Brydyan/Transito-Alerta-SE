c7a336955c0cf90b4f4c55464b6ed62786da0152
Andy Alejandro <andy.alejandrovera4290@upse.edu.ec>
2026-08-16 02:14:18 -0500

    fix(notifications): add UsersModule import for IncidentNotificationsListener
    IncidentNotificationsListener injects UsersService but NotificationsModule
    didn't import UsersModule. NestJS couldn't resolve dependency in E2E tests.
    
    Fixes: "Nest can't resolve dependencies of IncidentNotificationsListener"

--- backend/src/modules/notifications/notifications.module.ts
+++ backend/src/modules/notifications/notifications.module.ts
@@ -4,9 +4,10 @@ import { NotificationsService } from './notifications.service';
 import { NotificationsController } from './notifications.controller';
 import { Notification } from './entities/notification.entity';
 import { IncidentNotificationsListener } from './listeners/incident-notifications.listener';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Notification])],
+  imports: [TypeOrmModule.forFeature([Notification]), UsersModule],
   controllers: [NotificationsController],
   providers: [NotificationsService, IncidentNotificationsListener],
   exports: [NotificationsService],
