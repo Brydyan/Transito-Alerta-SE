db94337e0c1748cfbad6a3342bcabfd89287ac09
Andy Alejandro <andy.alejandrovera4290@upse.edu.ec>
2026-08-28 12:15:45 -0500

    chore: remove legacy GeoReporta project

--- GeoReporta/frontend/vitest.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vitest/config';
-import { allTestGlobs, sharedTestConfig } from './vitest.shared.js';
-
-export default defineConfig({
-  test: {
-    ...sharedTestConfig,
-    include: allTestGlobs,
-  },
-});
